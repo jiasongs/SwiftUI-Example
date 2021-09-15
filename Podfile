ec495532df02f4278f2887643db05ebafed321b0
jiasong <593908937@qq.com>
2021-09-15 17:10:31 +0800

    初始化项目

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, "13.0"
+use_frameworks! :linkage => :static
+
+target 'SwiftUI-Example' do
+  pod "LookinServer", "~> 1.0.4", :configurations => ["Debug"]
+end
+
+target 'SwiftUI-ExampleTests' do
+end
+
+target 'SwiftUI-ExampleUITests' do
+end
---
+++ SwiftUI-Example/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI-Example
+//
+//  Created by jiasong on 2021/9/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding(10)
+        Text("Hello, world!")
+            .padding(2)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
